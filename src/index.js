753a3014241eaf052ce4d29e2e514486b53143cb
Hamid Zare <dellydela@gmail.com>
2017-12-23 11:34:54 -0600

    basic express server with socketio

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const express = require('express'),
+  fse = require('fs-extra'),
+  bodyParser = require('body-parser'),
+  path = require('path'),
+  { exec } = require('child-process-promise'),
+  app = express(),
+  http = require('http').Server(app),
+  io = require('socket.io')(http);
+
+const port = process.env.PORT || 5000;
+
+// parse post body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// serve static files in public dir
+app.use(express.static(path.join(__dirname,'../public')))
+
+// listen for socket connections
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('submission', function(msg) {
+    console.log(msg);
+  })
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+})
+
+app.get('/ls', function(req, res) {
+  let commandJson = req.body;
+  // TODO process the command.
+
+  // linux cli can be accessed like this
+  exec('ls')
+    .then(res => {
+      console.log(res.stdout);
+    })
+    .catch(console.error)
+  // respond
+  // we can also wait for the robot to do its thing before replying.
+  res.status(200).send('command received.');
+});
+
+http.listen(port, function() {
+  console.log('Listening on ' + port);
+});
